54c40bc98dcaebc620cad8c16953e65f5eec5e7d
Kitt3120 <torben@schweren.dev>
2023-04-05 14:22:21 +0200

    Frontend docker scripts
    Added scripts for starting, stopping and compiling the frontend in docker for Windows and Unix.

---
+++ frontend/compileFrontend.ps1
@@ -0,0 +1,18 @@
+# Make sure user is in frontend directory
+$WD = Get-Location;
+$WD = Convert-Path $WD;
+
+# If $WD ends with "frontend"
+if(-not $WD.EndsWith("\frontend")) {
+    Write-Host "Please make sure you are in the frontend directory" -ForegroundColor Red;
+    exit;
+}
+
+# Build docker image
+docker build -t planty-frontend .
+
+# Run yarn install inside container
+docker run -it --rm --name planty-frontend-compiler -v ${WD}:/app planty-frontend yarn install
+
+# Run frontend inside container
+docker run -it --rm --name planty-frontend-compiler -v ${WD}:/app -p 4200:4200 planty-frontend ng build
